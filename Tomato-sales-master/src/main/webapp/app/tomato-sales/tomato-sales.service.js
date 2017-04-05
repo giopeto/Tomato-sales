3d4353c68f7c4d12e6bd4ce84426af1ddebde1b4
Georgy Georgiev <georg3georgiev@gmail.com>
2017-04-05 12:10:47 +0300

    Remove dir

--- Tomato-sales-master/src/main/webapp/app/tomato-sales/tomato-sales.service.js
+++
@@ -1,9 +0,0 @@
-ngApp.lazy.factory('TomatoFactory', function ($resource) {
-	var resource = $resource('tomatoes/data?size=:size', {size: '@size'}, {
-		update: {
-			method: 'PUT'
-		}
-	});
-
-	return resource;
-});
